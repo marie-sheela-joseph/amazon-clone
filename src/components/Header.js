54b7cd8b7282a235601fd7a722b406d74e54e987
Marie Sheela JOSEPH LOUIS SELVARAJ <sheelajoseph200@gmail.com>
2023-03-30 20:46:56 +0200

    header and banner added

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+import './Header.css';
+
+function Header() {
+    return (
+        <div>
+            <header className='header'>
+                <nav>
+                    <div className='header__logo'>
+                        <Link to={'/'}>
+                            <img src='./assets/logo.png' alt='amazon' />
+                        </Link>
+                    </div>
+                    <div className='header__search'>
+                        <input type={"text"} value="" />
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </div>
+                    <div className='header__nav'>
+                        <Link to={"/login"} className='header__link'>
+                            <div>
+                                <p className='header__optionLineOne'>Hello</p>
+                                <p className='header__optionLineTwo'>Sign out</p>
+                            </div>
+                        </Link>
+                        <Link to={"/checkout"} className='header__link'>
+                            <div className='header__cart'>
+                                <i class="fa-solid fa-cart-shopping"></i>
+                                <span className='header__optionLineTwo'>0</span>
+                            </div>
+                        </Link>
+                    </div>
+                </nav>
+            </header>
+            <Outlet />
+        </div>
+    )
+}
+export default Header
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Banner from './Banner';
+
+function Home() {
+    return (
+        <>
+            <Banner />
+        </>
+    );
+}
+export default Home
